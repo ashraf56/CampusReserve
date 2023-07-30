d4a35d844cd842ae21469f5457f9f3f3d38f9caf
ashraf56 <ashrafulfahim07@gmail.com>
2023-07-30 12:35:58 +0600

    r1

--- src/Admission/Admission.jsx
+++ src/Admission/Admission.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Admission = () => {
     return (
         <div>
-           <h1> All College admission</h1>
+           <div className='text-center'>
+            <img src="https://img.freepik.com/free-vector/construction-concept-illustration_114360-2764.jpg?w=740&t=st=1690698786~exp=1690699386~hmac=0e4bf85636026fd449e79125050ede693b648727ccf07f0c829c191c43df2ffc" className='w-3/6 mx-auto'/>
+           </div>
         </div>
     );
 };
--- src/Mycollege/Mycollege.jsx
+++ src/Mycollege/Mycollege.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Mycollege = () => {
     return (
         <div>
-           <h1>My College</h1>
-        </div>
+     <div className='text-center'>
+            <img src="https://img.freepik.com/free-vector/construction-concept-illustration_114360-2764.jpg?w=740&t=st=1690698786~exp=1690699386~hmac=0e4bf85636026fd449e79125050ede693b648727ccf07f0c829c191c43df2ffc" className='w-3/6 mx-auto'/>
+           </div>        </div>
     );
 };
 
